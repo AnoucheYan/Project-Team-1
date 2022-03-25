f1dc59800848e458997efea3114e9a0a3c554b7c
AnoucheYan <anush.aghamyan@gmail.com>
2022-03-26 01:35:35 +0400

    Added comment functional

--- src/routes/ticket.rout.js
+++ src/routes/ticket.rout.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const auth = require('../middlwares/jwtVerify');
+const commentController=require('../controllers/comment.controller');
 const ticketRouter = express.Router();
 const {create,deleteTicket, updateTicket, getTicket} = require('../controllers/ticket.controller');
   
@@ -22,6 +23,106 @@ ticketRouter.patch('/:id', auth, updateTicket);
 // delete single ticket
 ticketRouter.delete('/:id', auth, deleteTicket);
 
+/**
+ * @swagger
+ * components:
+ *   tags:
+ *    - name: Tickets
+ */
 
+/**
+ * @swagger
+ * /tickets/{ticketId}/comment:
+ *  put:
+ *   tags: [Tickets]
+ *   description: Comment ticket
+ *   parameters:
+ *     - in: header
+ *       name: x-access-token
+ *       schema:
+ *         type: string
+ *       required: true
+ *     - in: path
+ *       name: ticketId
+ *       schema:
+ *         type: string
+ *       required: true
+ *       description: Ticket id what user wants to comment
+ *   requestBody:
+ *     required: true
+ *     content:
+ *      application/json:
+ *        schema:
+ *          type: object
+ *          properties:
+ *            newComment:
+ *              type: string
+ *   responses:
+ *     200:
+ *       description: Comment added successfully
+ *       content:
+ *         application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              message:
+ *                type: string
+ *     401:
+ *       description: User not found or is not logged in
+ *       content:
+ *         application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ */
+
+ticketRouter.put('/:ticketId/comment', auth, commentController.commentTicket);
+
+/**
+ * @swagger
+ * /tickets/{ticketId}/comment:
+ *  get:
+ *   tags: [Tickets]
+ *   description: Get comments of ticket
+ *   parameters:
+ *     - in: header
+ *       name: x-access-token
+ *       schema:
+ *         type: string
+ *       required: true
+ *     - in: path
+ *       name: ticketId
+ *       schema:
+ *         type: string
+ *       required: true
+ *       description: Ticket id that comments user wants to get
+ *   responses:
+ *     200:
+ *       description: Comment added successfully
+ *       content:
+ *         application/json:
+ *          schema:
+ *            type: array
+ *            items:
+ *              type: object
+ *              properties:
+ *                userName:
+ *                  type: string
+ *                comment:
+ *                  type: string
+ *     401:
+ *       description: User not found or is not logged in
+ *       content:
+ *         application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              error:
+ *                type: string
+ */
+
+ticketRouter.get('/:ticketId/comment', auth, commentController.getComments);
 
 module.exports = ticketRouter;
